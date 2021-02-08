1313d272766efa428d539cbd0812dc8ff24d3b58
Co_Zax <sushibar2929@gmail.com>
2021-02-08 23:09:07 +0100

    Changement des images

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ const app = new PIXI.Application({width: window.innerWidth, height: window.inner
 document.getElementById("menu").appendChild(app.view);
 
 const img = new PIXI.Sprite.from("img/accueil.jpg");
-img.scale.set(1, 1);
-img.position.set(-45, -(app.screen.height / 3));
+// img.scale.set(1, 1);
+// img.position.set(-45, -1);
 img.alpha = .3;
 app.stage.addChild(img);
-console.log(img.width, img.height)
 
 const map = new PIXI.Sprite.from("img/accueil-map.jpg");
-map.scale.set(1, 1);
-map.position.set(-45, -(app.screen.height / 3));
+// map.scale.set(1, 1);
+// map.position.set(-45, 0);
 app.stage.addChild(map);
 
 const displacementFilter = new PIXI.filters.DisplacementFilter(map);
