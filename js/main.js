4762e2a1d7f61610873fdee13c9afe1519379556
wasaba <patafix258@gmail.com>
2021-02-10 19:55:41 +0100

    grands changements au niveau du html, menu terminé. Il manque encore la gestion du son qui se fera probablement grâce au localstorage.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // Le js, on rendra ça plus beau plus tard aussi
-const app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
 document.getElementById("menu").appendChild(app.view);
 
 const img = new PIXI.Sprite.from("img/accueil.jpg");
 // img.position.set(-45, -1);
-img.alpha = .3;
+img.alpha = 0.3;
 app.stage.addChild(img);
 
 const map = new PIXI.Sprite.from("img/accueil-map.jpg");
@@ -15,6 +18,6 @@ const displacementFilter = new PIXI.filters.DisplacementFilter(map);
 app.stage.filters = [displacementFilter];
 
 document.addEventListener("mousemove", (e) => {
-    displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 30;
-    displacementFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 30;
+  displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 30;
+  displacementFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 30;
 });
