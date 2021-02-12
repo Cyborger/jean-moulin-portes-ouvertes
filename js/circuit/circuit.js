08005181e5e127d5e9811471d410d6919064950c
Co_Zax <sushibar2929@gmail.com>
2021-02-12 19:12:26 +0100

    Changements: plan.js => plan.json

--- js/circuit/circuit.js
+++ js/circuit/circuit.js
@@ -1,2 +1,6 @@
-import plan from "./plan.js";
-console.dir(plan);
+async function load(){
+    const planRequest = await fetch("js/circuit/plan.json");
+    const plan = await planRequest.json();
+    console.log(plan)
+}
+load();
