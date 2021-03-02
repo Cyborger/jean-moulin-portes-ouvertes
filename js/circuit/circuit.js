e36a516c213b7876e2463c611c8b9a71fb8cdce5
Co_Zax <sushibar2929@gmail.com>
2021-03-02 15:50:49 +0100

    Changements mineurs circuit.html et circuit.js

--- js/circuit/circuit.js
+++ js/circuit/circuit.js
@@ -1,3 +1,12 @@
+// Fonctions
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
 // Charge le circuit
 async function load(){
     const currentURL = new URLSearchParams(window.location.search); // Récupère l'url actuelle
@@ -9,5 +18,16 @@ async function load(){
 
     const planRequest = await fetch("js/circuit/plan.json"); // Récupère le plan
     const plan = await planRequest.json();
+
+    const svgPaths = document.querySelectorAll("path");
+    const svgMain = document.querySelector("#svgContainer");
+
+    for (const svgPath of svgPaths){
+        svgPath.addEventListener("click", e => {
+
+            const _offset = offset(svgPath);
+            svgMain.style.transform = `translate(-${Math.floor(_offset.left)}px, ${Math.floor(_offset.top)}px) scale(2)`;
+        });
+    }
 }
 load();
